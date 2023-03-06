a153aae018a0e1513c329bfcfb2fc71210318a78
valfink <117605562+valfink@users.noreply.github.com>
2023-03-06 17:52:42 +0100

    Add test, doesn't fully work because of bad output of my endpoints ;)

---
+++ src/test/java/de/neuefische/cgn231springsecurity/controller/UserControllerTest.java
@@ -0,0 +1,50 @@
+package de.neuefische.cgn231springsecurity.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "user", password = "123")
+    void getMe_whenAuthenticated_thenUsername() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me_context"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "user", password = "123")
+    void getMe_whenRegisterd_thenInfos() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "username": "user",
+                                "password": "123"
+                                }
+                                """))
+                .andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("""
+                        user
+                        """));
+    }
+}
