c8f9062b7e33a743688855cba259e86a2d2e8d4c
valfink <117605562+valfink@users.noreply.github.com>
2023-03-06 09:54:14 +0100

    Fix dumb errors

--- src/main/java/de/neuefische/cgn231springsecurity/controller/MyController.java
+++ src/main/java/de/neuefische/cgn231springsecurity/controller/MyController.java
@@ -2,10 +2,11 @@ package de.neuefische.cgn231springsecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 @RequestMapping("/api/secret")
-public class RestController {
+public class MyController {
     @GetMapping
     public String secretEndpoint() {
         return "THIS IS TOTALLY SECRET!";
